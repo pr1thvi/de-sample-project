e08674170e219e2c355d4f5ac6d422b3c77482ec
Prithvi Marwadi <pmarwadi7@gmail.com>
2026-01-05 12:22:58 +0530

    dlt pipeline to ingest data from discover movie endpoint and load into duckdb

---
+++ dlt_duckdb/main.py
@@ -0,0 +1,26 @@
+import dlt
+from resources import movies
+
+
+@dlt.source
+def themoviedb_source(api_secret_key: str = dlt.secrets.value):
+    yield movies.themoviedb_movies_resource(api_secret_key)
+    
+    # yield dlt.resource(
+    #     movies.themoviedb_movie_details_resource(api_secret_key), name="movie_details"
+    # )
+
+
+if __name__ == "__main__":
+    """
+    Initializes and runs the data pipeline using the `dlt` library, fetching and processing data
+    from The Movie Database API. The pipeline is configured to load data into a `duckdb` database
+    """
+    pipeline = dlt.pipeline(
+        pipeline_name="themoviedb_pipeline",
+        destination="duckdb",
+        dataset_name="movie_data",
+        progress="alive_progress",
+    )
+    load_info = pipeline.run(themoviedb_source())
+    print(load_info)
