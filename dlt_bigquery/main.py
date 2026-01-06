0c9f4c6d7d70f56d4c6858fc302814841f74454e
Prithvi Marwadi <pmarwadi7@gmail.com>
2026-01-06 12:52:28 +0530

    load to google bigquery

---
+++ dlt_bigquery/main.py
@@ -0,0 +1,36 @@
+import dlt
+from resources import movies, trending, tv_series, genre
+
+@dlt.source
+def themoviedb_source(api_secret_key: str = dlt.secrets.value):
+    yield movies.themoviedb_movies_resource(api_secret_key)
+    
+    yield dlt.resource(
+        movies.themoviedb_movie_details_resource(api_secret_key), name="movie_details"
+    )
+
+    yield dlt.resource(
+        tv_series.themoviedb_tv_series_details_resource(api_secret_key), name="tv_series_details"
+    )
+
+    yield trending.themoviedb_trending_movies_resource(api_secret_key)
+    yield trending.themoviedb_trending_tv_series_resource(api_secret_key)
+
+    yield genre.themoviedb_genres_movies_resource(api_secret_key)
+    yield genre.themoviedb_genres_tv_series_resource(api_secret_key)
+
+
+if __name__ == "__main__":
+    """
+    Initializes and runs the data pipeline using the `dlt` library, fetching and processing data
+    from The Movie Database API. The pipeline is configured to load data into a `duckdb` database
+    """
+    pipeline = dlt.pipeline(
+        pipeline_name="themoviedb_pipeline",
+        destination="bigquery",
+        dataset_name="movie_data",
+        progress="alive_progress",
+    )
+    pipeline.drop()
+    load_info = pipeline.run(themoviedb_source())
+    print(load_info)
