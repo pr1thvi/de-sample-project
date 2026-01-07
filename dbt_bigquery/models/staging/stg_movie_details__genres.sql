328e0d1d7e2de1631e9028120a82c6a4d30341cf
Prithvi Marwadi <pmarwadi7@gmail.com>
2026-01-07 17:50:04 +0530

    setup dbt staging models

---
+++ dbt_bigquery/models/staging/stg_movie_details__genres.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='view') }}
+
+WITH source AS (
+    SELECT *
+    FROM {{ source('movie_data', 'movie_details__genres') }}
+)
+
+SELECT
+    id AS movie_genre_id,
+    name AS movie_genre_name,
+    _dlt_parent_id,
+    _dlt_id
+
+    {#- Unused Columns:
+    - _dlt_list_idx
+    #} 
+
+FROM source
